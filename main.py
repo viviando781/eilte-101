dc7c7322d416358af325b55e5941943143f819e1
viviando781 <viviando781@gmail.com>
2025-09-27 18:05:43 +0000

    Please enter the commit message for your changes. Lines starting

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 print("Welcome to the food ordering Chatbot");
 name = input("What is your name? ");
 age = input("How old are you, " + name + "? ");
+print("How can we help you?");
+print("1. Ordering");
+print("2. See the Menu");
+print("3. Refunds");
+print("4. Exit");
