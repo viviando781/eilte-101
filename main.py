b9fd8f1c9ef813c68be3b7e56900b8bf5c55ba6e
viviando781 <viviando781@gmail.com>
2025-10-08 16:52:34 -0500

    Update main1.py

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-print("Welcome to the food ordering Chatbot");
-name = input("What is your name? ");
-age = input("How old are you, " + name + "? ");
-print("How can we help you?");
-print("1. Ordering");
-print("2. See the Menu");
-print("3. Refunds");
-print("4. Exit");
+def main():
+    list1 = ['JIO68Y', 'GYF893', 'UGDUY1', 'GUI314', 'UISAD', 'HIOG53', 'BIVYU2', 'BYUT21']
+    name = input('What is your name? ')
+    print(f'Hi {name}, how can we help you?')
+    print('1. Returns')
+    print('2. Exchange')
+    
+    num = int(input("Enter 1 or 2: "))
+    
+    if num == 1:
+        receipt = input('Please enter your receipt number: ')
+        if receipt in list1:
+            print('We secussfuly got your order.')
+        else:
+            print('Sorry, we do not have your order.')
+    elif num = 2:
+        stuff = input('What do you want to exchange? ')
+        receipt2 = input('Please enter your receipt number: ')
+        if receipt2 in list1:
+            print('We secussfuly got your order.')
+        else:
+            print('Sorry, we do not have your order.')
+main()
